db3096ba7575a6b08945923bde04da8d43ac6cf0
Sagar Wadhwa <sagarwadhwa888@gmail.com>
2023-12-06 13:12:52 +0530

    Completed Week-8 Assignments

---
+++ Week-08/Assignment/InPlaceMergeSort.cpp
@@ -0,0 +1,43 @@
+// // IN PLACE MERGE SORT -> GAP METHOD
+// class Solution
+// {
+// public:
+//     void merge(vector<int> &nums, int &start, int &mid, int &end)
+//     {
+//         int total_len = end - start + 1;
+
+//         // Same as finding the CEIL OF A NUMBER
+//         int gap = (total_len / 2) + (total_len % 2);
+
+//         while (gap > 0)
+//         {
+//             int i = start, j = start + gap;
+//             while (j <= end)
+//             {
+//                 if (nums[i] > nums[j])
+//                 {
+//                     swap(nums[i], nums[j]);
+//                 }
+//                 i++;
+//                 j++;
+//             }
+//             gap = (gap <= 1) ? 0 : (gap / 2) + (gap % 2);
+//         }
+//     }
+//     void mergeSort(vector<int> &nums, int start, int end)
+//     {
+//         if (start >= end)
+//             return;
+
+//         int mid = start + ((end - start) >> 1);
+
+//         mergeSort(nums, start, mid);
+//         mergeSort(nums, mid + 1, end);
+//         merge(nums, start, mid, end);
+//     }
+//     vector<int> sortArray(vector<int> &nums)
+//     {
+//         mergeSort(nums, 0, nums.size() - 1);
+//         return nums;
+//     }
+// }
---
+++ Week-08/Assignment/QuickSort[EndElementAsPivot]-ByLakshayBhaiya.cpp
@@ -0,0 +1,1 @@
+// https://www.geeksforgeeks.org/problems/quick-sort/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=bottom_sticky_on_article
