89b05497bb60bc2a41c98eccb066234dc8d115b2
Sagar Wadhwa <sagarwadhwa726@yahoo.com>
2024-01-21 13:25:09 +0530

    Completed Graphs Lecture-1

---
+++ Week-19/Lecture-1.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <unordered_map>
+using namespace std;
+class Graph
+{
+public:
+    unordered_map<int, list<pair<int, int>>> adjList;
+
+    void addEdge(int u, int v, int weight, bool isDirected)
+    {
+        adjList[u].push_back({v, weight});
+        if (!isDirected)
+            adjList[v].push_back({u, weight});
+    }
+
+    void printAdjList()
+    {
+        for (auto node : adjList)
+        {
+            cout << node.first << "-> ";
+            for (auto neighbour : node.second)
+                cout << "(" << neighbour.first << "," << neighbour.second << "), ";
+            cout << "\n";
+        }
+    }
+};
+int main()
+{
+    system("cls");
+
+    // Representation of a graph using adjacency matrix
+    /*int n;
+    cout << "Enter the number of nodes in the graph : ";
+    cin >> n;
+
+    vector<vector<int>> adj(n, vector<int>(n, 0));
+
+    int e;
+    cout << "Enter the number of edges in the graph : ";
+    cin >> e;
+
+    for (int i = 0; i < e; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u][v] = 1;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << adj[i][j] << " ";
+        }
+        cout << "\n";
+    }*/
+
+    // // UNDIRECTED INPUT
+    // Graph g;
+    // g.addEdge(0, 1, 0);
+    // g.addEdge(1, 2, 0);
+    // g.addEdge(0, 2, 0);
+    // g.printAdjList();
+
+    // DIRECTED INPUT
+    Graph g;
+    g.addEdge(0, 1, 5, 1);
+    g.addEdge(1, 2, 8, 1);
+    g.addEdge(0, 2, 6, 1);
+    g.printAdjList();
+
+    return 0;
+}
---
+++ Week-19/Lecture-1_1.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <unordered_map>
+#include <queue>
+using namespace std;
+
+// We can now create a graph of any generic type
+// int, char, double etc.
+template <typename T>
+class Graph
+{
+public:
+    unordered_map<T, list<T>> adjList;
+
+    void addEdge(T u, T v, bool isDirected)
+    {
+        adjList[u].push_back(v);
+        if (!isDirected)
+            adjList[v].push_back(u);
+    }
+
+    void printAdjList()
+    {
+        for (auto node : adjList)
+        {
+            cout << node.first << "-> ";
+            for (auto neighbour : node.second)
+                cout << neighbour << ", ";
+            cout << "\n";
+        }
+    }
+
+    void bfs(T src, unordered_map<T, bool> &visited)
+    {
+        queue<T> q;
+
+        q.push(src);
+        visited[src] = true;
+
+        while (!q.empty())
+        {
+            T frontNode = q.front();
+            q.pop();
+            cout << frontNode << ", ";
+            visited[frontNode] = true;
+
+            // insert neighbours
+            for (auto neighbour : adjList[frontNode])
+            {
+                if (!visited[neighbour])
+                    q.push(neighbour);
+            }
+        }
+    }
+
+    void dfs(T src, unordered_map<T, bool> &visited)
+    {
+        cout << src << ", ";
+        visited[src] = true;
+
+        for (auto neighbour : adjList[src])
+        {
+            if (!visited[neighbour])
+                dfs(neighbour, visited);
+        }
+    }
+};
+int main()
+{
+    system("cls");
+
+    // // UNDIRECTED INPUT
+    // Graph g;
+    // g.addEdge(0, 1, 0);
+    // g.addEdge(1, 2, 0);
+    // g.addEdge(0, 2, 0);
+    // g.printAdjList();
+
+    // DIRECTED INPUT
+    // Graph g;
+    // g.addEdge(0, 1, 1);
+    // g.addEdge(1, 2, 1);
+    // g.addEdge(0, 2, 1);
+    // g.printAdjList();
+
+    // Graph<char> g;
+    // g.addEdge('a', 'b', 0);
+    // g.addEdge('b', 'c', 0);
+    // g.addEdge('a', 'c', 0);
+    // g.printAdjList();
+
+    Graph<int> g;
+    int n = 4;
+    g.addEdge(0, 1, 0);
+    g.addEdge(1, 3, 0);
+    g.addEdge(0, 2, 0);
+    g.addEdge(2, 4, 0);
+    g.printAdjList();
+    cout << endl;
+
+    unordered_map<int, bool> visited;
+    cout << "Printing BFS Traversal :\n";
+    for (int i = 0; i < n; i++)
+    {
+        if (!visited[i])
+        {
+            g.bfs(i, visited);
+            cout << "\n";
+        }
+    }
+
+    cout << "\n";
+    unordered_map<int, bool> visited2;
+    cout << "Printing DFS Traversal :\n";
+    for (int i = 0; i < n; i++)
+    {
+        if (!visited2[i])
+        {
+            g.dfs(i, visited2);
+            cout << "\n";
+        }
+    }
+    return 0;
+}
