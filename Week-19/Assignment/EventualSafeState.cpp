804efd8e158047fb520bc3deb30a1c59e40a033b
Sagar Wadhwa <sagarwadhwa888@gmail.com>
2024-01-31 10:29:26 +0530

    Completed 1 assignment question of graphs

---
+++ Week-19/Assignment/EventualSafeState.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    system("clear");
+    // https://leetcode.com/problems/find-eventual-safe-states/description/
+    // https://www.geeksforgeeks.org/problems/eventual-safe-states/1
+    return 0;
+}
