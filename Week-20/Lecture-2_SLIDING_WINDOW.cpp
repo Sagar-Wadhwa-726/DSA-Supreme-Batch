ef65ec968c808a00b5822dd7911cfddd8ab16468
Sagar Wadhwa <sagarwadhwa726@yahoo.com>
2024-02-05 17:06:37 +0530

    Completed Sliding Window Lecture

---
+++ Week-20/Lecture-2_SLIDING_WINDOW.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main()
+{
+    // SLIDING WINDOW MAXIMUM PROBLEM ON LEETCODE
+    // https://leetcode.com/problems/sliding-window-maximum/
+
+    // TWO SUM PROBLEM ON LEETCODE
+    // https://leetcode.com/problems/two-sum/description/
+
+    // MINIMUM SIZE SUBARRAY SUM PROBLEM ON LEETCODE
+    // https://leetcode.com/problems/minimum-size-subarray-sum/description/
+
+    // SMALLEST WINDOW IN A STRING CONTAINING ALL THE CHARACTERS OF ANOTHER STRING
+    // https://www.geeksforgeeks.org/problems/smallest-window-in-a-string-containing-all-the-characters-of-another-string-1587115621/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=bottom_sticky_on_article
+    // https://leetcode.com/problems/minimum-window-substring/description/
+    // https://leetcode.com/discuss/general-discussion/1122776/summary-of-sliding-window-patterns-for-subarray-substring
+    return 0;
+}
